342a204caa44f360e2f3476f396718d399529915
Aleksey Radionov <pretorean@gmail.com>
2021-11-08 00:50:24 +0300

    add coming soon screen

---
+++ lib/features/app/screens/coming_soon/coming_soon_screen.dart
@@ -0,0 +1,44 @@
+import 'package:elementary/elementary.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:ownfactory_flutter/features/app/screens/coming_soon/coming_soon_screen_wm.dart';
+
+class ComingSoonScreenRoute extends MaterialPageRoute<void> {
+  ComingSoonScreenRoute() : super(builder: (_) => const ComingSoonScreen());
+}
+
+class ComingSoonScreen extends ElementaryWidget<ComingSoonScreenWM> {
+  const ComingSoonScreen({
+    Key? key,
+    WidgetModelFactory wmFactory = createComingSoonScreenWM,
+  }) : super(wmFactory, key: key);
+
+  @override
+  Widget build(ComingSoonScreenWM wm) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Coming soon'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Icon(
+              Icons.broken_image,
+              size: 50,
+              color: Colors.indigo,
+            ),
+            SizedBox(height: 50),
+            Text(
+              'Coming soon',
+              style: TextStyle(
+                fontSize: 25,
+                color: Colors.indigo,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/features/app/screens/init_screen/init_screen_wm.dart
+++ lib/features/app/screens/init_screen/init_screen_wm.dart
@@ -2,8 +2,8 @@ import 'package:elementary/elementary.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:ownfactory_flutter/di/di_container.dart';
+import 'package:ownfactory_flutter/features/app/screens/coming_soon/coming_soon_screen.dart';
 import 'package:ownfactory_flutter/features/app/screens/init_screen/init_screen.dart';
-import 'package:ownfactory_flutter/ui/app/counter_app.dart';
 import 'package:ownfactory_flutter/utils/stub_model.dart';
 
 InitScreenWM createInitScreenWM(BuildContext context) {
@@ -26,11 +26,7 @@ class InitScreenWM extends WidgetModel<InitScreen, StubModel> {
     super.initWidgetModel();
 
     Future<void>.delayed(_splashDuration).then<void>(
-      (_) => _rootNavigator.pushReplacement(
-        MaterialPageRoute(
-          builder: (_) => const MyHomePage(title: 'Flutter Demo Home Page'),
-        ),
-      ),
+      (_) => _rootNavigator.pushReplacement(ComingSoonScreenRoute()),
     );
   }
 }
